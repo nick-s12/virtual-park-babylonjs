39f00c100809610b2f026b89757b575728500347
nick-s12 <seighitc06@gmail.com>
2025-09-24 20:59:13 +0300

    clean up code

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/', // <-- this is important!
+    base: '/',
     optimizeDeps: {
         include: ["@babylonjs/core", "@babylonjs/gui", "babylonjs", "babylonjs-charactercontroller"],
-        // exclude: ['../libs/CharacterController.mjs'], // don't prebundle this
     },
-    build: {
-        // commonjsOptions: {
-        //     transformMixedEsModules: true, // 👈 force-transform .mjs with require()
-        // },
-        // rollupOptions: {
-        //     external: ['../libs/CharacterController.mjs'], // keep it as a separate request
-        // },
-    }
+    build: {}
 });
 
